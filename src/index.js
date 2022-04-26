43019da19523433bda54752095f59edf0a3a184b
jodrack <drack.sir01@gmail.com>
2022-04-26 08:19:41 +0200

    Setup the index.js file to use the htmlwebpackplugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
